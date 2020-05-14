f3d6f3f771050cd1b38419bd98a39777c675424e
Bart <bart.wingelaar@gmail.com>
2020-05-14 21:30:28 +0200

    work

--- Experiments/2020-05-14 Experiment 3/ExperimentPreparation.m
+++ Experiments/2020-05-14 Experiment 3/ExperimentPreparation.m
@@ -2,24 +2,24 @@ clear all
 %%
 % control the valves
 % ValvesVecClosed = [LeakX1 x1x3Valve LeakX3 x3x2Valve LeakX2] % 1 means closed
-ValvesVecClosed = [1 1 1 1 1];
-ValveLD_close = ValvesVecClosed(1);
-ValveLM_close = ValvesVecClosed(2);
-ValveMD_close = ValvesVecClosed(3);
-ValveMR_close = ValvesVecClosed(4);
-ValveRD_close = ValvesVecClosed(5);
-ValveLD_open = double(~ValvesVecClosed(1));
-ValveLM_open = double(~ValvesVecClosed(2));
-ValveMD_open = double(~ValvesVecClosed(3));
-ValveMR_open = double(~ValvesVecClosed(4));
-ValveRD_open = double(~ValvesVecClosed(5));
-
-save('RL_TTS3_Controller_Blank.mat','ValveLD_close','ValveLM_close',...
-    'ValveMD_close','ValveMR_close','ValveRD_close','ValveLD_open',...
-    'ValveLM_open','ValveMD_open','ValveMR_open','ValveRD_open','-append');
-
-clear
-load('RL_TTS3_Controller_Blank.mat')
+% ValvesVecClosed = [1 1 1 1 1];
+% ValveLD_close = ValvesVecClosed(1);
+% ValveLM_close = ValvesVecClosed(2);
+% ValveMD_close = ValvesVecClosed(3);
+% ValveMR_close = ValvesVecClosed(4);
+% ValveRD_close = ValvesVecClosed(5);
+% ValveLD_open = double(~ValvesVecClosed(1));
+% ValveLM_open = double(~ValvesVecClosed(2));
+% ValveMD_open = double(~ValvesVecClosed(3));
+% ValveMR_open = double(~ValvesVecClosed(4));
+% ValveRD_open = double(~ValvesVecClosed(5));
+% 
+% save('RL_TTS3_Controller_Blank.mat','ValveLD_close','ValveLM_close',...
+%     'ValveMD_close','ValveMR_close','ValveRD_close','ValveLD_open',...
+%     'ValveLM_open','ValveMD_open','ValveMR_open','ValveRD_open','-append');
+% 
+% clear
+% load('RL_TTS3_Controller_Blank.mat')
 
 %% pump input
 clc
@@ -43,38 +43,38 @@ subplot(1,2,2); plot(t,cumsum(data1)*0.01/0.0154); title('level'); ylabel('mm');
 
 % total tank height
 trapz(t,data1/0.0154)
-
+return
 %% making inputs for experiment
 t2 = 0:Ts:max(t)*3;
 data2 = [data1 data1(2:end)*0 fliplr(data1(2:end))];
+
 subplot(1,2,1);
 plot(t2,data2); title('input')
 subplot(1,2,2); plot(t2,cumsum(data2)*0.01/0.0154); title('level'); ylabel('mm');yline(600); 
 
 % vectors for the valves
 ValveVec = [ones(1,size(t,2)) zeros(1,size(t,2)-1) ones(1,size(t,2)-1)];
-ValveVec = [ones(1,size(t,2))]>0;
+Valves_Vector = ValveVec==0; % 1 means open
 
-ValveLD_close_Vector = timeseries(ValveVec,t);
-ValveLM_close_Vector = timeseries(ValveVec,t);
-ValveMD_close_Vector = timeseries(ValveVec,t);
-ValveMR_close_Vector = timeseries(ValveVec,t);
-ValveRD_close_Vector = timeseries(ValveVec,t);
-ValveLD_open_vector = timeseries(double(~ValveVec),t);
-ValveLM_open_vector = timeseries(double(~ValveVec),t);
-ValveMD_open_vector = timeseries(double(~ValveVec),t);
-ValveMR_open_vector = timeseries(double(~ValveVec),t);
-ValveRD_open_vector = timeseries(double(~ValveVec),t);
+SetPump_vector = timeseries(data1,t);
+SetValves_vector = timeseries(Valves_Vector(1:32001),t);
 
-SetPumpR_vector =  timeseries((data1),t);
-SetPumpL_vector =  timeseries((data1),t);
-save('RL_TTS3_Controller_Blank.mat','SetPumpL_vector','SetPumpR_vector','-append');
-save('RL_TTS3_Controller_Blank.mat','ValveLD_close_Vector','ValveLM_close_Vector',...
-    'ValveMD_close_Vector','ValveMR_close_Vector','ValveRD_close_Vector',...
-    'ValveLD_open_vector','ValveLM_open_vector','ValveMD_open_vector',...
-    'ValveMR_open_vector','ValveRD_open_vector','-append');
-
-
-% clear
+% save('RL_TTS3_Controller_Blank.mat','ValveLD_close_Vector','ValveLM_close_Vector',...
+%     'ValveMD_close_Vector','ValveMR_close_Vector','ValveRD_close_Vector',...
+%     'ValveLD_open_vector','ValveLM_open_vector','ValveMD_open_vector',...
+%     'ValveMR_open_vector','ValveRD_open_vector','-append');
+% delete('ToRemoteLabs.zip')
+% zip('ToRemoteLabs',{'RL_TTS3_Controller_Blank.mat','RL_TTS3_Controller_Blank.slx'});
+% s=dir('ToRemoteLabs.zip').bytes/1000
+% % s=dir('ToRemoteLabs.zip')
+% clearvars
 % load('RL_TTS3_Controller_Blank.mat')
+
+%% store to zip file
+delete('ToRemoteLabs1.zip')
+delete('RL_TTS3_Controller_Blank.mat')
+save('RL_TTS3_Controller_Blank.mat','SetPump_vector','Ts','SetValves_vector')
+
+zip('ToRemoteLabs1',{'RL_TTS3_Controller_Blank.mat','RL_TTS3_Controller_Blank.slx'});
+kB=dir('ToRemoteLabs1.zip').bytes/1000
 
