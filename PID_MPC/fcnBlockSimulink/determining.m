b90adfe070ef47235dc7c9fb8829dd5d587e9357
MvHaren <maxvanharen@live.nl>
2020-06-02 17:25:32 +0200

    tuning + zip for exp.

--- PID_MPC/fcnBlockSimulink/determining.m
+++ PID_MPC/fcnBlockSimulink/determining.m
@@ -5,7 +5,7 @@ Ts = 0.1;
 [Act,Bct] = PID_fcn([0.3;0.3;0;0.5*Dvalve;0.5*Dvalve]);
 Q = [eye(3) zeros(3,2);
         zeros(2,5)];
-R = [eye(2) zeros(2);
+R = [0.05*eye(2) zeros(2);
      zeros(2) 0.001*eye(2)];
 [K,~,~] = lqrd(Act,Bct,Q,R,Ts); 
 save('RL_TTS3_Controller_Blank.mat','Ts','K','Dvalve');
