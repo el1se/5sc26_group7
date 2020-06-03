355e7a99765f8c279158ae080dcbdb2a1d89ab56
MvHaren <maxvanharen@live.nl>
2020-06-03 09:49:47 +0200

    improvements
    no list index out of range anymore!

--- PID_MPC/fcnBlockSimulink/determining.m
+++ PID_MPC/fcnBlockSimulink/determining.m
@@ -1,7 +1,7 @@
 %% constant
 S = 5e-5;               % [m^2]
 Dvalve = sqrt(4*S/pi);     % [m]
-Ts = 0.1;
+Ts = 0.01;
 [Act,Bct] = PID_fcn([0.3;0.3;0;0.5*Dvalve;0.5*Dvalve]);
 Q = [eye(3) zeros(3,2);
         zeros(2,5)];
