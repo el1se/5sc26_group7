f5b7e6a7ce285cb5caa37306a20621b42f3f81c2
MvHaren <maxvanharen@live.nl>
2020-06-03 11:32:15 +0200

    cleanup + data plotter

---
+++ PID_MPC/fcnBlockSimulink/dataPlotter.m
@@ -0,0 +1,66 @@
+% clear all; 
+close all; clc;
+load('Trajectory_tunedTubespeed.mat');
+t = mdfData.RunTime;
+X(:,1) = mdfData.ModelRoot_controller_controller_ModelRoot_ScopeHeightL_mm__In1;
+X(:,2) = mdfData.ModelRoot_controller_controller_ModelRoot_ScopeHeightR_mm__In1;
+X(:,3) = mdfData.ModelRoot_controller_controller_ModelRoot_ScopeHeightM_mm__In1;
+X(:,4) = mdfData.ModelRoot_controller_controller_ModelRoot_Valves_ScopeValveLM__;
+X(:,5) = mdfData.ModelRoot_controller_controller_ModelRoot_Valves_ScopeValveMR__;
+U(:,1) = mdfData.ModelRoot_controller_controller_ModelRoot_PumpR1_In1;
+U(:,2) = mdfData.ModelRoot_controller_controller_ModelRoot_PumpR_In1;
+
+LW = 1.3;
+figure
+subplot(2,1,1)
+plot(t,X(:,1),'Linewidth',LW+0.2); hold on;
+plot(t,X(:,2),'Linewidth',LW+0.2);
+plot(t,X(:,3),'Linewidth',LW+0.2);
+legend('Tank L','Tank R','Tank M');
+xlabel('Time [s]');
+ylabel('Tank heights [mm]')
+subplot(2,2,3)
+plot(t,X(:,4),'Linewidth',LW); hold on;
+plot(t,X(:,5),'Linewidth',LW);
+legend('Valve LM','Valve RM');
+xlabel('Time [s]');
+ylabel('Valve opening percentage [%]')
+subplot(2,2,4)
+plot(t,U(:,1),'Linewidth',LW); hold on;
+plot(t,U(:,2),'Linewidth',LW);
+legend('Pump L','Pump R')
+xlabel('Time [s]');
+ylabel('Pump input [L/s]');
+
+figure
+subplot(1,3,1)
+plot(t,X(:,1),'Linewidth',LW); hold on;
+plot(out.tout,out.simout.Data(:,1),'Linewidth',LW);
+xlabel('Time [s]');
+ylabel('Tank heights [mm]')
+subplot(1,3,2)
+plot(t,X(:,2),'Linewidth',LW); hold on;
+plot(out.tout,out.simout.Data(:,2),'Linewidth',LW);
+xlabel('Time [s]');
+ylabel('Tank heights [mm]')
+subplot(1,3,3)
+plot(t,X(:,3),'Linewidth',LW); hold on;
+plot(out.tout,out.simout.Data(:,3),'Linewidth',LW);
+xlabel('Time [s]');
+ylabel('Tank heights [mm]')
+legend('Real system','Simulation (tubeSpeed=0.0155)')
+
+
+figure
+subplot(1,3,1)
+plot(t,X(:,1)-out.simout.Data(:,1),'Linewidth',LW); 
+xlabel('Time [s]');
+ylabel('Residual between model and system [mm]')
+subplot(1,3,2)
+plot(t,X(:,2)-out.simout.Data(:,2),'Linewidth',LW); 
+xlabel('Time [s]');
+ylabel('Residual between model and system [mm]')
+subplot(1,3,3)
+plot(t,X(:,3)-out.simout.Data(:,3),'Linewidth',LW); 
+xlabel('Time [s]');
+ylabel('Residual between model and system [mm]')
--- PID_MPC/fcnBlockSimulink/determining.m
+++ PID_MPC/fcnBlockSimulink/determining.m
@@ -2,7 +2,7 @@
 S = 5e-5;               % [m^2]
 Dvalve = sqrt(4*S/pi);     % [m]
 Ts = 0.01;
-tubeSpeed = 0.01;
+tubeSpeed = 0.0155;
 [Act,Bct] = PID_fcn([0.3;0.3;0;0.5*Dvalve;0.5*Dvalve],tubeSpeed);
 Q = [eye(3) zeros(3,2);
         zeros(2,5)];
