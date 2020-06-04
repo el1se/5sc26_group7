df4e81e66514ab85e0bfce2bd01c073045a4c77e
MvHaren <maxvanharen@live.nl>
2020-06-04 09:56:39 +0200

    Major improvements

--- MPC_TTS/fcnBlockSimulink/prepperMPC.m
+++ MPC_TTS/fcnBlockSimulink/prepperMPC.m
@@ -1,5 +1,15 @@
-tubeSpeed = 0.0155;
+tubeSpeed = 0.0155; clc;
 Ts = 0.01;
 Ts2 = 2;
+S = 5e-5;               % [m^2]
+Dvalve = sqrt(4*S/pi);     % [m]
+[Atemp,Btemp] =  PID_fcn([0.3;0.3;0.15;0.5*Dvalve;0.5*Dvalve],0.0155);
+Q = [eye(3) zeros(3,2);
+        zeros(2,5)];
+R = [0.5*eye(2) zeros(2,2);
+    zeros(2,2) 0.00005*eye(2)];
+[~,P,~] = lqrd(Atemp,Btemp,Q,R,Ts2);
+
+
 save('RL_TTS3_Controller_Blank.mat','Ts');
-save('nonTunables.mat','Ts2','tubeSpeed');
+save('nonTunables.mat','Ts2','tubeSpeed','Q','R','P');
