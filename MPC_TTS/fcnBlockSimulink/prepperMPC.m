36aa85ec7ec87c79bb9c7a0bb6cb11a5a28157fa
MvHaren <maxvanharen@live.nl>
2020-06-16 16:48:16 +0200

    MPC update new model

--- MPC_TTS/fcnBlockSimulink/prepperMPC.m
+++ MPC_TTS/fcnBlockSimulink/prepperMPC.m
@@ -3,7 +3,8 @@ Ts = 2;
 Ts2 = Ts;
 S = 5e-5;               % [m^2]
 Dvalve = sqrt(4*S/pi);     % [m]
-[Atemp,Btemp] =  PID_fcn([0.3;0.3;0.15;0.5*Dvalve;0.5*Dvalve],tubeSpeed);
+betaBar = [16.1461609250188,15.5165993538781]./1000;
+[Atemp,Btemp] =  PID_fcn([0.3;0.3;0.15;0.5*Dvalve;0.5*Dvalve],betaBar);
 Q = [eye(3) zeros(3,2);
         zeros(2,5)];
 R = [0.005*eye(2) zeros(2,2); % 0.5
