1c42d0dbba82a4470c5be887af661770d1ca5e1d
MvHaren <maxvanharen@live.nl>
2020-06-17 10:35:11 +0200

    Update MPC for runability

--- MPC_TTS/fcnBlockSimulink/prepperMPC.m
+++ MPC_TTS/fcnBlockSimulink/prepperMPC.m
@@ -1,10 +1,10 @@
 tubeSpeed = 0.0155; clc;
-Ts = 2;
+Ts = 0.5;
 Ts2 = Ts;
 S = 5e-5;               % [m^2]
 Dvalve = sqrt(4*S/pi);     % [m]
-betaBar = [16.1461609250188,15.5165993538781]./1000;
-[Atemp,Btemp] =  PID_fcn([0.3;0.3;0.15;0.5*Dvalve;0.5*Dvalve],betaBar);
+bB = [16.1461609250188,15.5165993538781]./1000;
+[Atemp,Btemp] =  PID_fcn([0.2;0.2;0.1;0.5*Dvalve;0.5*Dvalve],bB);
 Q = [eye(3) zeros(3,2);
         zeros(2,5)];
 R = [0.005*eye(2) zeros(2,2); % 0.5
@@ -13,5 +13,5 @@ R = [0.005*eye(2) zeros(2,2); % 0.5
 
 
 save('RL_TTS3_Controller_Blank.mat','Ts');
-save('nonTunables.mat','Ts2','tubeSpeed','Q','R','P');
+save('nonTunables.mat','Ts2','tubeSpeed','Q','R','P','bB');
 load('RL_busses.mat');
